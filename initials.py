185060290881194ec1f1c68b1101d1745f00b69d
obengkhosta14 <obengkhosta@gmail.com>
2026-05-31 22:16:03 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
-
+# File: initials.py
+# Description: Prints stylized large block initials with surrounding formatting.
+# Assignment Number: 1
+#
+# Name: BRIGHT OBENG APPIAH
+# STUDENT ID: 2525402043
+# Email: 2425402043@gctu.edu.gh
+# Grader: AUGUTUS BUCKMAN
+#
+# On my honor, BRIGHT OBENG APPIAH, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
-
+    """Prints the small initials and stylized large block initials JPS."""
+    # Blank line before the small initials
+    print()
+    
+    # Small initials line
+    print("...BOA")
+    
+    # Blank line between small and large initials
+    print()
+    
+    # Large initials - 10 lines of 60 characters total width
+    print("......BBBBBBBBBBB............OOOOOOOOOOOOO....................AAAAAAA..............")
+    print("......BBB......BBB.........OOOOOOOOOOOOOOOOO.................AAAAAAAAA.............")
+    print("......BBB.......BBB.......OOOO...........OOOO...............AAAA...AAAA............")
+    print("......BBB......BBB........OOOO...........OOOO..............AAAA.....AAAA...........")
+    print("......BBBBBBBBBB..........OOOO...........OOOO.............AAAA.......AAAA..........")
+    print("......BBBBBBBBBB..........OOOO...........OOOO............AAAAAAAAAAAAAAAAA.........")
+    print("......BBB......BBB........OOOO...........OOOO...........AAAAAAAAAAAAAAAAAAA........")
+    print("......BBB.......BBB.......OOOO...........OOOO..........AAAA.............AAAA.......")
+    print("......BBB......BBB...**....OOOOOOOOOOOOOOOOO....**....AAAA...............AAAA....**")
+    print("......BBBBBBBBBBB....**......OOOOOOOOOOOOO......**...AAAA.................AAAA...**")
+    
+    # Blank line after the large initials
+    print()
 
 
 main()
